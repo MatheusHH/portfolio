1b662e76c840061fabce32cd2e00c86c8844b907
Matheus Henrique <matheussilva770@gmail.com>
2020-01-21 17:02:50 -0300

    create block controller, update model block adding a custom validator and set position to new blocks that are added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   rescue_from Pundit::NotAuthorizedError do
     respond_to do |format|
       format.html { redirect_to root_path, flash: { error: 'Unauthorized' } }
-      format.json { render json: { error: 'Unauthorized' } }
+      format.json { render json: { errors: ['Unauthorized'] }, status: :unauthorized }
     end
   end
  
