253fcfe1b10a395170ad7d744a0f8d42fc61b4ad
Matheus Henrique <matheussilva770@gmail.com>
2020-02-10 21:18:18 -0300

    add tests to skills, create controller and add vue view

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     resources :profiles, only: [:index, :update]
     resources :experiences, only: [:index, :create, :destroy]
     resources :educations, only: [:index, :create, :destroy]
+    resources :skills, only: [:index, :create, :destroy]
   end
  
   resources :tags, only: :index
